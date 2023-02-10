155c947241dc76e79558598bf5147fb7f222f6eb
Kevin Hermawan <84965338+kevinhermawan@users.noreply.github.com>
2023-02-10 21:57:59 +0700

    feat: adds script suggestions when the script is not found

--- src/errors.ts
+++ src/errors.ts
@@ -14,15 +14,22 @@ export const CONFIG_FILE_NOT_JSON_ERROR = () => {
 
 export const CONFIG_FILE_NOT_FOUND_ERROR = (file: string) => {
   console.error(
-    red(`Missing config file. Please ensure that '${file}' file exists.`),
+    red(`Missing config file. Please ensure that '${file}' file exists.`)
   );
 
   process.exit(1);
 };
 
-export const SCRIPT_NAME_NOT_FOUND_ERROR = (name: string) => {
+export const SCRIPT_NAME_NOT_FOUND_ERROR = (
+  name: string,
+  suggestion: string
+) => {
   console.error(
-    red(`The script '${name}' could not be found in the config file.`),
+    red(
+      suggestion.length > 0
+        ? `The script "${name}" could not be found in the config file. Did you mean "${suggestion}"?`
+        : `The script "${name}" could not be found in the config file.`
+    )
   );
 
   process.exit(1);
@@ -31,8 +38,8 @@ export const SCRIPT_NAME_NOT_FOUND_ERROR = (name: string) => {
 export const DIRECTORY_NOT_FOUND_ERROR = (name: string) => {
   console.error(
     red(
-      `The directory '${name}' could not be found. Please make sure that there is no spelling or typing error.`,
-    ),
+      `The directory '${name}' could not be found. Please make sure that there is no spelling or typing error.`
+    )
   );
 
   process.exit(1);
--- src/runner.ts
+++ src/runner.ts
@@ -1,3 +1,4 @@
+import { closest } from "fastest-levenshtein";
 import { spawn } from "node:child_process";
 import { existsSync, readFileSync } from "node:fs";
 
@@ -21,18 +22,22 @@ export function runner({ script, configFile }: Args) {
   if (existsSync(configFile)) {
     const configString = readFileSync(configFile, "utf-8");
     const configJSON = JSON.parse(configString);
-    const scriptToRun = configJSON?.scripts?.[script];
+    const scripts = configJSON?.scripts;
+    const scriptToRun = scripts?.[script];
 
     if (scriptToRun) {
-      const command = scriptToRun.split(" ")[0];
+      const cmd = scriptToRun.split(" ")[0];
       const args = scriptToRun.split(" ").slice(1);
-      const child = spawn(command, args, { shell: true, stdio: "inherit" });
+      const child = spawn(cmd, args, { shell: true, stdio: "inherit" });
 
       child.on("error", (error) => {
         ANY_ERROR(error.message);
       });
     } else {
-      SCRIPT_NAME_NOT_FOUND_ERROR(script);
+      SCRIPT_NAME_NOT_FOUND_ERROR(
+        script,
+        closest(script, Object.keys(scripts))
+      );
     }
   } else {
     CONFIG_FILE_NOT_FOUND_ERROR(configFile);
